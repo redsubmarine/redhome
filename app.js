d4649d3f3e508b11b98a7c506f7e2c5124149b6f
kevin <kevin.yang@peeper.co>
2017-10-19 01:10:24 +0900

    path 사용, 간단 css 적용.

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.listen(3000, function() {
     console.log('Server started.');
